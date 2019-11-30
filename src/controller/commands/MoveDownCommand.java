74b211945ad98e9f11c2fe3fd2d72a58b2e49877
Matas Liutikas <matasliutikas@gmail.com>
2019-12-01 00:51:30 +0200

    extracted code to methods

--- src/controller/commands/MoveDownCommand.java
+++ src/controller/commands/MoveDownCommand.java
@@ -3,6 +3,7 @@ package controller.commands;
 import controller.GameLogic;
 import controller.TetrominoConveyor;
 import model.Grid;
+import model.tetromino.Tetromino;
 
 public class MoveDownCommand implements CommandInterface {
 
@@ -11,6 +12,7 @@ public class MoveDownCommand implements CommandInterface {
     private final GameLogic gameLogic;
 
     public MoveDownCommand(TetrominoConveyor conveyor, Grid grid, GameLogic gameLogic) {
+        // Todo injetc dependencies
         this.conveyor = conveyor;
         this.grid = grid;
         this.gameLogic = gameLogic;
@@ -19,21 +21,32 @@ public class MoveDownCommand implements CommandInterface {
     @Override
     public void execute() {
         var tetromino = conveyor.getCurrent();
-
         tetromino.moveDown();
-        if (gameLogic.hasCollided(grid, tetromino)) {
-            tetromino.moveUp();
-
-            gameLogic.imprintTetromino(grid, tetromino);
-            gameLogic.removeFilledLines(grid);
-
-            if (gameLogic.hasCollided(grid, conveyor.getNext())) {
-                gameLogic.setGameOver();
-            } else {
-                conveyor.move();
-            }
-        }
-
-
+        handleCollisionAndGameOver(tetromino);
     }
+
+    private void handleCollisionAndGameOver(Tetromino tetromino) {
+        if (hasCollided(tetromino)) {
+            tetromino.moveUp();
+            removeFilledLines(tetromino);
+            ifCannotSpawnNextSetGameOver();
+            conveyor.move();
+        }
+    }
+
+    private void ifCannotSpawnNextSetGameOver() {
+        if (hasCollided(conveyor.getNext())) {
+            gameLogic.setGameOver(true);
+        }
+    }
+
+    private void removeFilledLines(Tetromino tetromino) {
+        gameLogic.imprintTetromino(grid, tetromino);
+        gameLogic.removeFilledLines(grid);
+    }
+
+    private boolean hasCollided(Tetromino tetromino) {
+        return gameLogic.hasCollided(grid, tetromino);
+    }
+
 }
