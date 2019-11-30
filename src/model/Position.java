78ebc36432b31bbf0de0cfb20748ebb7d336e201
Matas Liutikas <matasliutikas@gmail.com>
2019-11-30 15:13:01 +0200

    removed unused code

--- src/model/Position.java
+++ src/model/Position.java
@@ -4,11 +4,6 @@ public class Position {
     public int x;
     public int y;
 
-    public Position(Position position) {
-        this.x = position.x;
-        this.y = position.y;
-    }
-
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
